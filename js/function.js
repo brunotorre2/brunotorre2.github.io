4961218399a24e289abc0e5c7169e8f94e96ba31
brunotorre2 <84163093+brunotorre2@users.noreply.github.com>
2023-02-28 18:12:12 -0300

    Initial commit

---
+++ js/function.js
@@ -0,0 +1,70 @@
+$("#python").hover(
+    function() {
+        $(this).append($("<span> ⮕ 85% </span>"));
+    },
+    
+    function(){
+        $(this).find("span").last().remove();
+    }
+    );
+
+$("#html5").hover(
+    function() {
+        $(this).append($("<span> ⮕ 100% </span>"));
+    },
+    
+    function(){
+        $(this).find("span").last().remove();
+    }
+    );
+
+$("#css3").hover(
+    function() {
+        $(this).append($("<span> ⮕ 95% </span>"));
+    },
+    
+    function(){
+        $(this).find("span").last().remove();
+    }
+    );
+
+$("#javascript").hover(
+    function() {
+        $(this).append($("<span> ⮕ 90% </span>"));
+    },
+    
+    function(){
+        $(this).find("span").last().remove();
+    }
+    );
+
+$("#reactjs").hover(
+    function() {
+        $(this).append($("<span> ⮕ 70% </span>"));
+    },
+    
+    function(){
+        $(this).find("span").last().remove();
+    }
+    );
+
+$("#php").hover(
+    function() {
+        $(this).append($("<span> ⮕ 90% </span>"));
+    },
+    
+    function(){
+        $(this).find("span").last().remove();
+    }
+    );
+
+$("#mysql").hover(
+    function() {
+        $(this).append($("<span> ⮕ 90% </span>"));
+    },
+    
+    function(){
+        $(this).find("span").last().remove();
+    }
+    );
+
